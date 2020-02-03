cb0f8572b20e8ab0cb225aebc0997abb2d63b78a
Quinn McCourt <quinn.mccourt@gmail.com>
2020-02-02 18:48:56 -0600

    save

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,4 +15,6 @@ export class AppComponent {
     text: `Everybody's favorite racists are back for more. Word on the street is, they've taken the fight to the pavement against Vogel's, Herandez's, and the Big D's campagin HQs. Plus the usual harassment of metahuman neighborhoods in smaller burgs throughout the UCAS.`,
     storyID: 1
   };
+
+  comment = `>>>[Airlink intown is fast but expensive, around 45 nuyen fixed rate. Stick to cabs, chummer. If you really need air transport in the city, rent it. Airlinks to other parts of PCC are pretty reasonable, though, like 100-200 nuyen, dependig on distance.]>>>`
 }
